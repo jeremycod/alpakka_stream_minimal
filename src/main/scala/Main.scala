02da5e4af55ba16b23d7108daf81b5ac61b6c2aa
Zoran Jeremic <zoran.jeremic@yoppworks.com>
2021-02-02 07:37:04 -0800

    scalafmt

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,10 +1,16 @@
 import akka.actor.ActorSystem
 import akka.stream.alpakka.cassandra.CassandraSessionSettings
-import akka.stream.alpakka.cassandra.scaladsl.{CassandraSession, CassandraSessionRegistry}
+import akka.stream.alpakka.cassandra.scaladsl.{
+  CassandraSession,
+  CassandraSessionRegistry
+}
+
 object Main extends App {
   implicit val system = ActorSystem("Operationfusion")
   // #cassandra-setup
   val sessionSettings = CassandraSessionSettings()
-  implicit val cassandraSession: CassandraSession = CassandraSessionRegistry.get(system).sessionFor(sessionSettings)
+
+  implicit val cassandraSession: CassandraSession =
+    CassandraSessionRegistry.get(system).sessionFor(sessionSettings)
 
 }
